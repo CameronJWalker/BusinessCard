46b2779e323e901a247c14d4cfdf9bf19f4f6f8c
CameronJWalker <cameron.jr.walker@icloud.com>
2023-02-17 18:53:07 -0800

    added photo, background, buttons, and some text and styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function About() {
     return(
-        <div className="about">
-            About me
+        <div className="about-section">
+            <img className="about-photo" alt=""/>
+            <h1 className="name">Cameron Walker</h1>
+            <h2 className="title">Full-Stack Developer</h2>
+            <button className ="email">Email</button>
+            <button className ="linkedin">LinkedIn</button>
         </div>
     )
 }
